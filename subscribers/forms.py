42a263211629488292ce33db0c86fa205a4ea94d
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-06-03 19:23:33 +0200

    created the subscriber view

--- subscribers/forms.py
+++ subscribers/forms.py
@@ -1,9 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
-class ContactForm(forms.ModelForm):
-	search_query = forms.CharField(max_length=100)
-
 class SubscriberForm(UserCreationForm):
 	email = forms.EmailField(
 		required=True, 
@@ -18,3 +15,16 @@ class SubscriberForm(UserCreationForm):
 	password2 = forms.CharField(
 		wiget=forms.TextInput(attrs={'class':'inner', 'type': 'password'})
 		)
+
+    # def clean_username(self):
+    # try:
+    #     user = User.objects.get(username__iexact=self.cleaned_data['username'])
+    # except User.DoesNotExist:
+    #     return self.cleaned_data['username']
+    # raise forms.ValidationError(_("The username already exists. Please try another one."))
+
+    # def clean(self):
+    #     if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+    #         if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+    #             raise forms.ValidationError(_("The two password fields did not match."))
+    #     return self.cleaned_data
--- subscribers/views.py
+++ subscribers/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
-from .forms import ContactForm
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
-def search(request):
-	if request.method == 'POST':
-		form = ContactForm(request.POST)
-		if form.is_valid():
-			s_query = form.cleaned_data['search_query']
-			s_results = SomeTable.objects.filter(name=s_query)
-			return render(request, 'search.html', {'form': form, 's_results': s_results})
-	else:
-		form = SearchForm()
+from .forms import SubscriberForm
 
-	return render(request, 'search.html', {'form': form})
+def subscriber_new(request, template='subscribers/subscriber_new.html'):
+    if request.method == 'POST':
+        form = SubscriberForm(request.POST)
+        if form.is_valid():
+
+            # Unpack form values
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            email = form.cleaned_data['email']
+
+            # Create the User record
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+
+            # Create Subscriber Record
+            # Process payment (via Stripe)
+            # Auto login the user
+            return HttpResponseRedirect('/success/')
+    else:
+        form = SubscriberForm()
+
+    return render(request, template, {'form':form})
