c21370800af7792c63ca533bc74c60ae038ab75c
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-06-05 19:45:40 +0200

    updated the subscriber view to use processing code

--- subscribers/views.py
+++ subscribers/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 
+from django.contrib.auth import authenticate, login
+from django.forms.forms import NON_FIELD_ERRORS
+from django.conf import settings
+from django.core.urlresolvers import reverse
+
 from .forms import SubscriberForm
 from .models import Subscriber
+
+import stripe
 
 def subscriber_new(request, template='subscribers/subscriber_new.html'):
     if request.method == 'POST':
@@ -30,10 +37,33 @@ def subscriber_new(request, template='subscribers/subscriber_new.html'):
             sub = Subscriber(address_one=address_one, address_two=address_two,
                 city=city, state=state, user_rec=user)
             sub.save()
+
             # Process payment (via Stripe)
+            fee = settings.SUBSCRIPTION_PRICE
+            try:
+                stripe_customer = sub.charge(request, email, fee)
+            except stripe.StripeError as e:
+                form._errors[NON_FIELD_ERRORS] = form.error_class([e.args[0]])
+                return render(request, template,
+                    {'form':form,
+                     'STRIPE_PUBLISHABLE_KEY':settings.STRIPE_PUBLISHABLE_KEY}
+                )
+            
             # Auto login the user
-            return HttpResponseRedirect('/success/')
+            a_u = authenticate(username=username, password=password)
+            if a_u is not None:
+                if a_u.is_active:
+                    login(request, a_u)
+                    return HttpResponseRedirect(reverse('account_list'))
+                else:
+                    return HttpResponseRedirect(
+                        reverse('django.contrib.auth.views.login')
+                    )
+            else:
+                return HttpResponseRedirect(reverse('sub_new'))
     else:
         form = SubscriberForm()
 
-    return render(request, template, {'form':form})
+    return render(request, template,
+    {'form':form,
+     'STRIPE_PUBLISHABLE_KEY':settings.STRIPE_PUBLISHABLE_KEY})
