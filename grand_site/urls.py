cb486428b721a59c8308e4e034d3da198e4595f2
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-06-07 18:31:39 +0200

    create login & logout URLs

--- grand_site/urls.py
+++ grand_site/urls.py
@@ -32,7 +32,13 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     # Login/Logout URLs
+    url(r'^login/$',
+    'django.contrib.auth.views.login', {'template_name': 'login.html'}
+    ),
 
+    url(r'^logout/$',
+        'django.contrib.auth.views.logout', {'next_page': '/login/'}
+    ),
 
     # Account related URLs
 
