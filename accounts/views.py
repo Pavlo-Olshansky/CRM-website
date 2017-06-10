03f8202d3b665fd6593dab24a3bc07bc10f89b4e
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-06-10 21:12:18 +0200

    added new account view

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,14 @@ from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
+from django.http import HttpResponseForbidden
+from django.shortcuts import render
+from django.http import HttpResponseRedirect 
+from django.core.urlresolvers import reverse
+
 from .models import Account
+from .forms import AccountForm
+
 
 class AccountList(ListView):
     model = Account
@@ -29,8 +36,7 @@ class AccountList(ListView):
         return super(AccountList, self).dispatch(*args, **kwargs)
 
 
-from django.http import HttpResponseForbidden
-from django.shortcuts import render
+
 
 @login_required()
 def account_detail(request, uuid):
@@ -44,3 +50,30 @@ def account_detail(request, uuid):
     }
 
     return render(request, 'accounts/account_detail.html', variables)
+
+
+
+@login_required()
+def account_cru(request):
+
+    if request.POST:
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            account = form.save(commit=False)
+            account.owner = request.user
+            account.save()
+            redirect_url = reverse(
+                'grand_site.accounts.views.account_detail',
+                args=(account.uuid,)
+            )
+            return HttpResponseRedirect(redirect_url)
+    else:
+        form = AccountForm()
+
+    variables = {
+        'form': form,
+    }
+
+    template = 'accounts/account_cru.html'
+
+    return render(request, template, variables)
