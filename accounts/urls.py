06c82d111c966cdf725134d73b8e29cb4e6383d2
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-06-11 14:08:29 +0200

    added edit account URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from .views import account_detail
+from .views import account_detail, account_update
 
 
 urlpatterns = [
 
+	url(r'^edit/$', account_update, name='account_update'),
+	
     url(r'^$', account_detail, name='account_detail'),
 
 ]
