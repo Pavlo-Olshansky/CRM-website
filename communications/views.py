f99a22a551198dbd53ed5c4382e478d9f06f18bd
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-06-13 18:36:57 +0200

    created the edit communication view

--- communications/views.py
+++ communications/views.py
@@ -5,8 +5,11 @@ from django.http import HttpResponseForbidden
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
+from django.shortcuts import get_object_or_404
+
 from .models import Communication
 from .forms import CommunicationForm  
+from crmapp.accounts.models import Account
 
 
 @login_required()
@@ -20,10 +23,17 @@ def comm_detail(request, uuid):
 
 
 @login_required()
-def comm_cru(request):
+def comm_cru(request, uuid=None, account=None):
+
+    if uuid:
+        comm = get_object_or_404(Communication, uuid=uuid)
+        if comm.owner != request.user:
+            return HttpResponseForbidden()
+    else:
+        comm = Communication(owner=request.user)
 
     if request.POST:
-        form = CommunicationForm(request.POST)
+        form = CommunicationForm(request.POST, instance=comm)
         if form.is_valid():
             # make sure the user owns the account
             account = form.cleaned_data['account']
@@ -36,15 +46,22 @@ def comm_cru(request):
             # return the user to the account detail view
             reverse_url = reverse('account_detail', 
             	args=(account.uuid,))
-
-            return HttpResponseRedirect(reverse_url)
+        else:
+            # if the form isn't valid, still fetch the account so it can be passed to the template
+            account = form.cleaned_data['account']
     else:
-        form = CommunicationForm()
+        form = CommunicationForm(instance=comm)
+
+    # this is used to fetch the account if it exists as a URL parameter
+    if request.GET.get('account', ''):
+        account = Account.objects.get(id=request.GET.get('account', ''))
 
     variables = {
         'form': form,
+        'comm':comm,
+        'account': account
     }
-
+    
     template = 'communications/comm_cru.html'
 
     return render(request, template, variables)
