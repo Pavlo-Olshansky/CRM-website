cc603eec4e069e6518b4a5788f993303dcfbd928
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-06-13 11:17:54 +0200

    created edit contact url

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
-from .views import contact_detail
+from .views import contact_detail, contact_cru
 
 
 urlpatterns = [
 
     url(r'^$', contact_detail, name='contact_detail'),
+
+    url(r'^edit/$', contact_cru, name='contact_update'),
+
 
 ]
