bf84eda5b1be285d44604eab2f89e9e93fd2b8cf
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-06-13 18:35:19 +0200

    created the edit communication URL

--- communications/urls.py
+++ communications/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from .views import comm_detail
+from .views import comm_detail, comm_cru
 
 urlpatterns = [
 
 	url(r'^$', comm_detail, name="comm_detail"),
+
+    url(r'^edit/$', comm_cru, name='comm_update'),
 	
 ]
