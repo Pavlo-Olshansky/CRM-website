1e7908af2ca205715b0f7d32f44d40c77f919dd6
Pavlo Olshansky <pavlo.olshansky@gmail.com>
2017-06-13 11:24:14 +0200

    modified contact view to allow for editing

--- contacts/views.py
+++ contacts/views.py
@@ -4,10 +4,12 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseForbidden
+from django.shortcuts import get_object_or_404
 
 
 from .models import Contact
 from .forms import ContactForm
+from accounts.models import Account  
 
 
 @login_required()
@@ -22,10 +24,17 @@ def contact_detail(request, uuid):
 
 
 @login_required()
-def contact_cru(request):
+def contact_cru(request, uuid=None, account=None):
+
+    if uuid:
+        contact = get_object_or_404(Contact, uuid=uuid)
+        if contact.owner != request.user:
+            return HttpResponseForbidden()
+        else:
+            contact = Contact(owner=request.user)
 
     if request.POST:
-        form = ContactForm(request.POST)
+       form = ContactForm(request.POST, instance=contact)
         if form.is_valid():
             # make sure the user owns the account
             account = form.cleaned_data['account']
@@ -38,11 +47,20 @@ def contact_cru(request):
             # return the user to the account detail view
             reverse_url = reverse('account_detail', args=(account.uuid,))
             return HttpResponseRedirect(reverse_url)
+        else:
+            # if the form isn't valid, still fetch the account so it can be passed to the template
+            account = form.cleaned_data['account']
     else:
-        form = ContactForm()
+        form = ContactForm(instance=contact)
+
+    # this is used to fetch the account if it exists as a URL parameter
+    if request.GET.get('account', ''):
+        account = Account.objects.get(id=request.GET.get('account', ''))
 
     variables = {
         'form': form,
+        'contact': contact,
+        'account': account
     }
 
     template = 'contacts/contact_cru.html'
